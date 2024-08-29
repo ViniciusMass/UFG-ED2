9b5c977f3d4f184a67d0a7fc61a0f0dc80569bc6
ViniciusMass <136811892+ViniciusMass@users.noreply.github.com>
2024-08-29 15:36:07 -0300

    Update 01-Vetores.c

--- RevisãoED1/01-Vetores.c
+++ RevisãoED1/01-Vetores.c
@@ -7,17 +7,24 @@ O código abaixo realiza a impressão do vetor na ordem inversa.
 #include <stdio.h>
 
 int main() {
-    int vetor[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int i, temp;
-
-    for (i = 0; i < 5; i++) {
-        temp = vetor[i];
-        vetor[i] = vetor[9 - i];
-        vetor[9 - i] = temp;
+    int n, i, temp; // Definição das variáveis
+    
+    printf("Tamanho do vetor: \n"); 
+    scanf("%d", &n); // Recebe o tamanho 'n' do vetor
+    int vetor[n];
+    
+    for (i = 0; i < n; i++) { // Declara os valores do vetor, onde: vetor[n] = {0, 1, 2, 3 ... n}
+        vetor[i] = i; 
+    }
+    
+    for (i = 0; i < n/2; i++) { // Passa até a metade do vetor, invertindo sua ordem
+        temp = vetor[i]; // Guarda o valor na posição i na variável 'temp'
+        vetor[i] = vetor[n - 1 - i]; // Troca o valor na posição i com o valor na sua posição 'simétrica'
+        vetor[n - 1 - i] = temp; // Troca o valor na posição 'simétrica' de i
     }
 
-    printf("Vetor invertido:\n");
-    for (i = 0; i < 10; i++) {
+    printf("Vetor invertido:\n"); 
+    for (i = 0; i < n; i++) { // Imprime o vetor novo, invertido, posição por posição
         printf("%d ", vetor[i]);
     }
 
