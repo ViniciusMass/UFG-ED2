eb25177393eb7e228b9876d4c6d262c0dda9ab36
ViniciusMass <136811892+ViniciusMass@users.noreply.github.com>
2024-08-31 21:27:10 -0300

    Update 03-ListaLinear.c

--- RevisãoED1/03-ListaLinear.c
+++ RevisãoED1/03-ListaLinear.c
@@ -13,26 +13,26 @@ ou
 int lista[MAX_SIZE];
 int tamanho = 0;
 
-void inserir(int elemento) {
+void inserir(int elemento) { // Insere um dado elemento na primeira posição disponível da lista
     if (tamanho < MAX_SIZE) {
         lista[tamanho++] = elemento;
-    } else {
+    } else { // Caso 'tamanho' da lista seja maior que o MAX_SIZE definido acima (3), ela n insere o elemento.
         printf("Lista cheia!\n");
     }
 }
 
-void remover(int index) {
+void remover(int index) { // Remove um certo elemento localizado numa posição dada da lista
     if (index < 0 || index >= tamanho) {
-        printf("Índice inválido!\n");
+        printf("Índice inválido!\n"); // Não aceita uma posição que não existe
     } else {
         for (int i = index; i < tamanho - 1; i++) {
-            lista[i] = lista[i + 1];
+            lista[i] = lista[i + 1]; // Passa todos os elementos à frente do índice removido uma posição para trás
         }
         tamanho--;
     }
 }
 
-void imprimir() {
+void imprimir() { // Imprime todos os elementos da lista em ordem
     printf("Elementos da lista:\n");
     for (int i = 0; i < tamanho; i++) {
         printf("%d ", lista[i]);
@@ -40,7 +40,7 @@ void imprimir() {
     printf("\n");
 }
 
-void buscar(int elemento) {
+void buscar(int elemento) { // Procura um dado elemento, imprimindo a posição que ele ocupa na lista
     int aux = 0;
     for (int i = 0; i < tamanho; i++) {
         if (elemento == lista[i]){
@@ -49,16 +49,66 @@ void buscar(int elemento) {
         }
     }
     if (aux == 0) {
-        printf("Não existe esse elemento na lista.\n");
+        printf("Não existe esse elemento na lista.\n"); // Caso esse elemento não exista
+    }
+}
+
+void inverter() { // Inverte a ordem da lista
+    int i, temp;
+    for (i = 0; i < tamanho/2; i++) { // Passa até a metade do vetor, invertindo sua ordem
+        temp = lista[i]; // Guarda o valor na posição i na variável 'temp'
+        lista[i] = lista[tamanho - 1 - i]; // Troca o valor na posição i com o valor na sua posição 'simétrica'
+        lista[tamanho - 1 - i] = temp; // Troca o valor na posição 'simétrica' de i
+    }
+}
+
+void encontrarMaximo() { // Encontra o maior valor entre todos os elementos e o imprime, junto à posição que ele ocupa na lista
+    int temp = lista[0];
+    for(int i = 1; i < tamanho; i++) {
+        if(lista[i] >= temp) {
+            temp = lista[i];
+        }
+    }
+    printf("Valor máximo: %d na(s) posição(ões):\n", temp);
+    buscar(temp);
+}
+
+void encontrarMinimo() { // Encontra o menor valor entre todos os elementos e o imprime, junto à posição que ele ocupa na lista
+    int temp = lista[0];
+    for(int i = 1; i < tamanho; i++) {
+        if(lista[i] <= temp) {
+            temp = lista[i];
+        }
+    }
+    printf("Valor mínimo: %d na(s) posição(ões):\n", temp);
+    buscar(temp);
+}
+
+void ordenar() { // Coloca a lista em ordem crescente de valores
+    int i, j, temp;
+    for (i = 0; i < tamanho-1; i++) {
+        for (j = 0; j < tamanho-i-1; j++) {
+            if (lista[j] > lista[j+1]) {
+                temp = lista[j];
+                lista[j] = lista[j+1];
+                lista[j+1] = temp;
+            }
+        }
     }
 }
 
 int main() {
-    inserir(10);
     inserir(20);
+    inserir(10);
     inserir(30);
     buscar(10);
     buscar(15);
+    imprimir();
+    encontrarMaximo();
+    encontrarMinimo();
+    inverter();
+    imprimir();
+    ordenar();
     imprimir();
     remover(1);
     imprimir();
