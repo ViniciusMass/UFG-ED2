f52b78ed7be1e56a94530c36fc7607985293c927
ViniciusMass <136811892+ViniciusMass@users.noreply.github.com>
2024-09-02 11:49:04 -0300

    Update 04-ListasDuplamenteLigadas.c

--- RevisãoED1/04-ListasDuplamenteLigadas.c
+++ RevisãoED1/04-ListasDuplamenteLigadas.c
@@ -9,7 +9,7 @@ O código apresenta uma lista duplamente ligada com operações básicas para in
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
+typedef struct Node { // Definição da lista
     int data;
     struct Node* next;
     struct Node* prev;
@@ -17,7 +17,7 @@ typedef struct Node {
 
 Node* head = NULL;
 
-void insertFront(int value) {
+void insertFront(int value) { // Insere um dado valor á lista
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = value;
     newNode->next = head;
@@ -27,7 +27,7 @@ void insertFront(int value) {
     head = newNode;
 }
 
-void deleteNode(Node* node) {
+void deleteNode(Node* node) { // Deleta um dado nó da lista
     if (node == head)
         head = node->next;
     if (node->next != NULL)
@@ -37,7 +37,7 @@ void deleteNode(Node* node) {
     free(node);
 }
 
-void printList() {
+void printList() { // Imprime a lista inteira
     Node* temp = head;
     while (temp != NULL) {
         printf("%d ", temp->data);
@@ -46,9 +46,72 @@ void printList() {
     printf("\n");
 }
 
+void buscarValor(int valor) { // Busca um dado valor
+    Node* temp = head;
+    while (temp != NULL) {
+        if (temp->data == valor) {
+            printf("Valor %d encontrado!\n", valor);
+            printf("Detalhes do nó:\n");
+            printf("Valor: %d\n", temp->data);
+            if (temp->prev != NULL) { // Imprime o valor anterior, caso esse exista (se o valor buscado não for o primeiro nó da lista)
+                printf("Anterior: %d\n", temp->prev->data);
+            } else {
+                printf("Este é o primeiro nó da lista.\n");
+            }
+            if (temp->next != NULL) { // Imprime o próximo valor, caso esse exista (se o valor buscado não for o último nó da lista)
+                printf("Próximo: %d\n", temp->next->data);
+            } else {
+                printf("Este é o último nó da lista.\n");
+            }
+            return;
+        }
+        temp = temp->next;
+    }
+    printf("Valor %d não encontrado na lista.\n", valor); // Caso o valor buscado não existir na lista.
+}
+
 int main() {
-    insertFront(10);
-    insertFront(20);
+    int valor, operacao;
+    
+    while (1) {
+        printf("O que deseja fazer?\n0-Sair\n1-Inserir valor\n2-Buscar Valor\n3-Remover nó\n"); // "Menu" de opções para permitir a busca, inserção e remoção de valores
+        scanf("%d", &operacao);
+        switch(operacao){
+            case 0:
+                break;
+            case 1:
+                printf("Digite o valor que deseja inserir: ");
+                scanf("%d", &valor);
+                insertFront(valor);
+                printf("Valor inserido!\n");
+                break;
+            case 2:
+                printf("Digite o valor que deseja buscar: ");
+                scanf("%d", &valor);
+                buscarValor(valor);
+                break;
+            case 3:
+                printf("Digite o valor que deseja deletar: ");
+                scanf("%d", &valor);
+                Node* temp = head;
+                while (temp != NULL && temp->data != valor) {
+                    temp = temp->next;
+                }
+                if (temp != NULL) {
+                    deleteNode(temp);
+                    printf("Valor removido!\n");
+                } else {
+                    printf("Valor nao encontrado!\n");
+                }
+                break;
+            default:
+                printf("Operação não reconhecida.\n");
+        }
+        if(operacao==0){
+            break;
+        }
+    }
+    
     printList();
     return 0;
 }
